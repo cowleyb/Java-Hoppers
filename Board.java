92380dfff473ed23a2b40e3ad2fd3f07cfeb3a6b
Cowley <cowleyb@egb070000028.lancs.local>
2020-03-16 19:28:37 +0000

    Setting up classes

--- Board.java
+++ Board.java
@@ -5,11 +5,28 @@ public class Board
 {
     private JFrame frame;
     private JPanel panel;
+    private Square[][] squares;
+    
 
     public Board()
     {
         frame = new JFrame();
+        frame.setTitle("Hoppers");
         frame.setSize(500,500);
+
+        panel = new JPanel();
+        panel.setLayout(new GridLayout(5,5));
+        frame.add(panel);
+
+        squares = new Square[5][5];
+        for (int i=0; i<5; i++) {
+            for (int j=0; j< 5;j++){
+                squares[i][j]=new Square(i,j,0);
+            }
+        }
+
+
+
         frame.setVisible(true);
     }
 
---
+++ Square.java
@@ -0,0 +1,58 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Square 
+{
+    private int state;
+    private int x;
+    private int y;
+    private JPanel panel;
+    private JButton button;
+
+    public Square(int px,int py,int pstate)
+    {
+        state = pstate;
+        x = px;
+        y = py;
+        panel = new JPanel();
+        button = new JButton();
+
+        panel.add(button);
+        this.update("Water.png");
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public int getState()
+    {
+        return state;
+    }
+
+    public void setState(int pstate)
+    {
+        state = pstate;
+    }
+    
+    public void update(String icon)
+    {
+        ImageIcon i = new ImageIcon(icon);
+        button.setIcon(i);
+    }
+
+
+
+
+
+
+
+
+
+}
