2eaa0cf6337c4173d80a42ed1a54f73727850707
Cowley <cowleyb@egb070000028.lancs.local>
2020-03-16 18:32:29 +0000

    Setting up classes

---
+++ Board.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Board
+{
+    private JFrame frame;
+    private JPanel panel;
+
+    public Board()
+    {
+        frame = new JFrame();
+        frame.setSize(500,500);
+        frame.setVisible(true);
+    }
+
+}
+
+
